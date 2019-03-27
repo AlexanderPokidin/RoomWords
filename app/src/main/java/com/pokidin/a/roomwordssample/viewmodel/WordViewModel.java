4b612c43ed269cbdd80dff7a154dfd26ab91eef8
AlexanderPokidin <a.pokidin@gmail.com>
2019-03-27 14:21:59 +0200

    ViewModel added

---
+++ app/src/main/java/com/pokidin/a/roomwordssample/viewmodel/WordViewModel.java
@@ -0,0 +1,31 @@
+package com.pokidin.a.roomwordssample.viewmodel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import com.pokidin.a.roomwordssample.entity.Word;
+import com.pokidin.a.roomwordssample.repo.WordRepository;
+
+import java.util.List;
+
+public class WordViewModel extends AndroidViewModel {
+
+    private WordRepository mRepository;
+    private LiveData<List<Word>> mAllWords;
+
+    public WordViewModel(@NonNull Application application) {
+        super(application);
+        mRepository = new WordRepository(application);
+        mAllWords = mRepository.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords() {
+        return mAllWords;
+    }
+
+    public void insert(Word word) {
+        mRepository.insert(word);
+    }
+}
