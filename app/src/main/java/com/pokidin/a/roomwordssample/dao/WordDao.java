3a300d41222fe049ceb0ab369377268dfbeed533
AlexanderPokidin <a.pokidin@gmail.com>
2019-03-27 12:55:24 +0200

    DB and repository added

--- app/src/main/java/com/pokidin/a/roomwordssample/dao/WordDao.java
+++ app/src/main/java/com/pokidin/a/roomwordssample/dao/WordDao.java
@@ -1,11 +1,11 @@
-package com.pokidin.a.roomwordssample.DAO;
+package com.pokidin.a.roomwordssample.dao;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 
-import com.pokidin.a.roomwordssample.Entity.Word;
+import com.pokidin.a.roomwordssample.entity.Word;
 
 import java.util.List;
 
--- app/src/main/java/com/pokidin/a/roomwordssample/entity/Word.java
+++ app/src/main/java/com/pokidin/a/roomwordssample/entity/Word.java
@@ -1,4 +1,4 @@
-package com.pokidin.a.roomwordssample.Entity;
+package com.pokidin.a.roomwordssample.entity;
 
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
