df19b7b568c52225ca1b15664c404c4c8ab1bfe2
AlexanderPokidin <a.pokidin@gmail.com>
2019-03-30 14:37:41 +0200

    RoomDatabase callback added

--- app/src/main/java/com/pokidin/a/roomwordssample/db/WordRoomDatabase.java
+++ app/src/main/java/com/pokidin/a/roomwordssample/db/WordRoomDatabase.java
@@ -1,9 +1,12 @@
 package com.pokidin.a.roomwordssample.db;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 import com.pokidin.a.roomwordssample.dao.WordDao;
 import com.pokidin.a.roomwordssample.entity.Word;
@@ -24,10 +27,40 @@ public abstract class WordRoomDatabase extends RoomDatabase {
                             WordRoomDatabase.class, "word_database")
                             // Wipes and rebuilds instead of migrating if no Migration object
                             .fallbackToDestructiveMigration()
+                            .addCallback(sCallback)
                             .build();
                 }
             }
         }
         return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback sCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+            new PopulateDbAsync(INSTANCE).execute();
+        }
+    };
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+        private final WordDao mWordDao;
+        String[] words = {"dolphin", "crocodile", "cobra"};
+
+        PopulateDbAsync(WordRoomDatabase db) {
+            mWordDao = db.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            // Start the app with a clean database every time.
+            // Not needed if you only populate the database when it is first created
+            mWordDao.deleteAll();
+            for (int i = 0; i < words.length - 1; i++) {
+                Word word = new Word(words[i]);
+                mWordDao.insert(word);
+            }
+            return null;
+        }
     }
 }
